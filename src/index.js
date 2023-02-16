8c6b83eb8e23bbe35519047a785fcba38162dc2c
inki <inkinyam@yandex.ru>
2023-02-16 16:28:07 +0300

    prerender

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, hydrateRoot  } from 'react-dom/client';
 import './index.scss';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-  <HashRouter >
-    <App />
-  </HashRouter>
-</React.StrictMode>
-);
+const container = document.getElementById('root');
 
-/*заменить при деплое на нормальный сервер!!!!*/ 
+if (container.hasChildNodes()) {
+   hydrateRoot(container, <BrowserRouter> <App/> </BrowserRouter>);
+} else {
+ const root =  createRoot(container);
+  root.render(<BrowserRouter> <App/> </BrowserRouter>)
+}
+
 
 reportWebVitals();
